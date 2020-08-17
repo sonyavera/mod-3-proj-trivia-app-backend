ca7f655a3c22ebb2fe3712f95a74960bdb0fe473
sonyavera <sonyavgould@gmail.com>
2020-08-17 15:06:41 -0400

    Setting up quiz controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,11 @@
 class QuizzesController < ApplicationController
+    
+    def create(category, difficulty)  
+           
+    end
+    # create quiz here
+    
+    def show
+        #quiz is rendered here
+    end
 end
--- app/models/trivia_api.rb
+++ app/models/trivia_api.rb
@@ -5,13 +5,41 @@ require 'json'
 
 class TriviaApi
 
-    def self.get_categories
-        response = HTTParty.get("https://opentdb.com/api_category.php")
-        test = JSON.parse(response.body)
-        puts test
+    def self.generate_quiz(category, difficulty)
+        category_id = get_category_id(category)
+        response = HTTParty.get("https://opentdb.com/api.php?amount=10&category=#{category_id}&difficulty=#{difficulty}&type=multiple")
+        questions_json = JSON.parse(response.body)["results"]
+        # questions_json.map do |question|
+        #     question = {question, correct_answer, incorrect_answers}
+        # end
+        # we should be using destructuring here
+        # get quiz data, render quiz data
+        # decide when to move over to javascript
     end
 
-    
+    def self.create_categories
+        response = HTTParty.get("https://opentdb.com/api_category.php")
+        category_list = JSON.parse(response.body)
+        category_arr = []
+        category_list["trivia_categories"].each do |category|
+            category_arr.push(category["name"])
+        end
+    end
+
+    def self.get_category_id(category)
+        response = HTTParty.get("https://opentdb.com/api_category.php")
+        category_json = JSON.parse(response.body)
+        category = category_json["trivia_categories"].find do |cat|
+                        cat["name"] == "#{category}"
+                   end 
+        category_id = category["id"]
+        return category_id
+    end
+
+
 end
 
-TriviaApi.get_categories
+# TriviaApi.create_categories
+# TriviaApi.create_quizzes
+# TriviaApi.get_category_id("Mythology")
+TriviaApi.generate_quiz("Mythology", "medium")
